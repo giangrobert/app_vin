2e66b6250ab1b41d7247641e7d5525d3e49e8e68
Gian Grobert Mamani Mamani <gianmamani93@gmail.com>
2023-12-05 17:55:09 -0500

    add confirm

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -4,6 +4,8 @@ import {RouterOutlet} from "@angular/router";
 import {MatIconModule} from "@angular/material/icon";
 import {abcForms} from "../../../environments/generals";
 import {MatButtonModule} from "@angular/material/button";
+import {UntypedFormBuilder, UntypedFormGroup} from "@angular/forms";
+import {FuseConfirmationService} from "../../../@fuse/services/confirmation";
 
 
 @Component({
@@ -15,12 +17,53 @@ import {MatButtonModule} from "@angular/material/button";
 })
 export class DashboardComponent {
     public title: string = '';
+    configForm: UntypedFormGroup;
     abcForms:any;
-    constructor() {
+    constructor( private _formBuilder: UntypedFormBuilder,private _fuseConfirmationService: FuseConfirmationService) {
     }
     ngOnInit() {
         this.title = 'Dashboard'
         this.abcForms = abcForms;
+    }
+    configConfirm(){
+        this.configForm = this._formBuilder.group({
+            title      : 'Eliminar nodo',
+            message    : `
+                ¿Estás seguro de que deseas eliminar este nodo permanentemente?
+            `,
+            icon       : this._formBuilder.group({
+                show : true,
+                name : 'heroicons_outline:exclamation-triangle',
+                color: 'warn',
+            }),
+            actions    : this._formBuilder.group({
+                confirm: this._formBuilder.group({
+                    show : true,
+                    label: 'Eliminar',
+                    color: 'warn',
+                }),
+                cancel : this._formBuilder.group({
+                    show : true,
+                    label: 'Cancelar',
+                }),
+            }),
+            dismissible: true,
+        });
+    }
+
+
+    dialog(){
+        this.configConfirm();
+        const dialogRef = this._fuseConfirmationService.open(this.configForm.value);
+
+        // Confirm delete node
+        dialogRef.afterClosed().subscribe((result) =>
+        {
+            if ("confirmed" == result) {
+                alert("no confirmado");
+            }
+            // this.deleteNode(node);
+        });
     }
 
 }
