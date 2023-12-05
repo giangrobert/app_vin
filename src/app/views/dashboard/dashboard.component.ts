f0b1bc0c7344790ce494e5a128fc2287bc7099b5
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-12-05 17:47:16 -0500

    update dashboard.component.ts

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,26 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterOutlet} from "@angular/router";
-import {MatIconModule} from "@angular/material/icon";
-import {abcForms} from "../../../environments/generals";
-import {MatButtonModule} from "@angular/material/button";
-
+import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { abcForms } from '../../../environments/generals';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MatIconModule, MatButtonModule],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+    selector: 'app-dashboard',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, MatIconModule, MatButtonModule],
+    templateUrl: './dashboard.component.html',
+    //   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
     public title: string = '';
-    abcForms:any;
-    constructor() {
-    }
+    abcForms: any;
+    constructor() {}
     ngOnInit() {
-        this.title = 'Dashboard'
+        this.title = 'Dashboard';
         this.abcForms = abcForms;
     }
-
 }
