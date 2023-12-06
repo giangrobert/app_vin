69702b3ba3e860dbad086dda0d3f1da50ffc8744
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-12-06 10:41:56 -0500

    update 1 file and create 1 file update period.service.ts

--- src/app/providers/utils/end-points.ts
+++ src/app/providers/utils/end-points.ts
@@ -1,35 +1,35 @@
 export const END_POINTS = {
-  oauth: {
-    register: 'auth/register',
-    login: 'auth/login',
-    logout: 'auth/logout',
-    signup: 'api/auth/register',
-    active: 'auth/active',
-    create: 'auth/create',
-    find: 'find',
-    reset: 'auth/reset',
-  },
-  setup: {
-    role: 'rol',
-    person: 'persona',
-    institution: 'institucion',
-    subsidiary: 'filial',
-    faculty: 'facultad',
-    prefessionalSchool: 'escuela-profesional',
-    cycle: 'ciclo',
-    group: 'grupo',
-    jerarquia: "gerarquia",
-    menu:"menu"
-  },
-  event: {
-    event: 'evento',
-  },
-  attendance: {
-    attendance: 'asistencia',
-  },
-  /*****---------- */
-  userModules: 'user-modules',
-  validTokensOauth: 'valid-tokens-oauth',
-  orders: 'messages',
+    oauth: {
+        register: 'auth/register',
+        login: 'auth/login',
+        logout: 'auth/logout',
+        signup: 'api/auth/register',
+        active: 'auth/active',
+        create: 'auth/create',
+        find: 'find',
+        reset: 'auth/reset',
+    },
+    setup: {
+        role: 'rol',
+        person: 'persona',
+        institution: 'institucion',
+        subsidiary: 'filial',
+        faculty: 'facultad',
+        prefessionalSchool: 'escuela-profesional',
+        cycle: 'ciclo',
+        group: 'grupo',
+        jerarquia: 'gerarquia',
+        menu: 'menu',
+        users: 'users',
+    },
+    event: {
+        event: 'evento',
+    },
+    attendance: {
+        attendance: 'asistencia',
+    },
+    /*****---------- */
+    userModules: 'user-modules',
+    validTokensOauth: 'valid-tokens-oauth',
+    orders: 'messages',
 };
-
