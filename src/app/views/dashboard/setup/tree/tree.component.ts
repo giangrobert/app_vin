446a88d0d80670b50d82e1d9e74cd3f8878e024c
hectorjavi <hectorjavilimahuaya@gmail.com>
2023-12-06 16:34:37 -0500

    Crud tree completado

--- src/app/views/dashboard/setup/tree/tree.component.ts
+++ src/app/views/dashboard/setup/tree/tree.component.ts
@@ -1,34 +1,7 @@
-// import {Component, OnInit} from '@angular/core';
-// import {abcForms} from "../../../../../environments/generals";
-// @Component({
-//   template: `
-//     <div class="card shadow-gm-card m-1">
-//       <h1 class="fa-3x icon-gm-float"><i class="{{abcForms.btnUser.icon}}"></i></h1>
-//       <div class="card-body">
-//         <router-outlet></router-outlet>
-//       </div>
-//     </div>
-//   `,
-// })
-// export class RolesComponent implements OnInit {
-//
-//   public title: string = '';
-//   abcForms:any;
-//   constructor() {
-//   }
-//
-//   ngOnInit() {
-//     this.title = 'Roles';
-//     this.abcForms = abcForms;
-//   }
-//
-// }
-
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { abcForms } from '../../../../../environments/generals';
 import { Component, OnInit } from '@angular/core';
-import { TreeContainerComponent } from './containers/tree-container.component';
 
 @Component({
     selector: 'app-tree',
@@ -44,7 +17,6 @@ import { TreeContainerComponent } from './containers/tree-container.component';
             </div>
         </div>
     `,
-    // styleUrl: './setup.component.scss'
 })
 export class TreeComponent implements OnInit {
     public title: string = '';
