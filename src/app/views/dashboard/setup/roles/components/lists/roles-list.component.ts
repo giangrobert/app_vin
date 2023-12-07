2da97ca8744cec52c08acfdc72bc908ee32262c0
Gian Grobert Mamani Mamani <gianmamani93@gmail.com>
2023-12-07 08:44:30 -0500

    input

--- src/app/views/dashboard/setup/roles/components/lists/roles-list.component.ts
+++ src/app/views/dashboard/setup/roles/components/lists/roles-list.component.ts
@@ -135,6 +135,9 @@ import { MatDialog } from '@angular/material/dialog';
                                             (click)="goDelete(r.id)"
                                             >delete_sweep</mat-icon
                                         >
+                                        <button (click)="goAssign(user.id)">
+                                        <mat-icon>swap_horiz</mat-icon>
+                                    </button>
                                         <!--                                        <mat-icon-->
                                         <!--                                            class="text-sky-400 hover:text-sky-600 cursor-pointer"-->
                                         <!--                                            (click)="openComposeDialog()"-->
